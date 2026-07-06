76d1ea63008557776997772980a81741dded1e37
Mohamed Abdelfatah <abdelfata7new@gmail.com>
2026-07-06 22:05:52 +0300

    connect EF with Sql

--- MLProject/MLProject/Program.cs
+++ MLProject/MLProject/Program.cs
@@ -1,8 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using MlBL.Services;
+using MlBL.ServicesInterfaces;
+using MlDAL.DbContexts;
+using MlDAL.Interfaces;
+using MlDAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IAnalysisRepository, AnalysisRepo>();
+builder.Services.AddScoped<IAnalysisService, AnalysisService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
